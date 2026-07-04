53ce4249d8902e670974c25cec3a077ca59d8f4a
PaytonDennis <102490243+PaytonDennis@users.noreply.github.com>
2026-07-04 14:33:05 -0600

    removed oop practice file, updated dependencies

--- backend/src/main/java/com/example/backend/oopPractice.java
+++ backend/src/main/java/com/example/backend/oopPractice.java
@@ -1,45 +1,12 @@
 package com.example.backend;
 
-import java.lang.reflect.Array;
-import java.util.ArrayList;
-
 public class oopPractice {
 
-    public static String getMainMessage() {
-        return "OOP Practice!";
+    public String getMainMessage() {
+        return "Hello from backend! ";
     }
 
-    public static String displayMessage() {
-        return "!";
+    public String displayMessage() {
+        return "This is the message from oopPractice.";
     }
-
-    public static void main(String[] args) {
-        ArrayList<Integer> numbersA = new ArrayList<Integer>();
-
-        numbersA.add(1);
-        numbersA.add(2);
-        numbersA.add(3);
-        System.out.println("Numbers in ArrayList: " + numbersA);
-
-    }
-
-    public static void doStuff() {
-        int a = 10;
-        int b = 20;
-
-        if (b > a) {
-            System.out.println("b is greater than a");
-        } else {
-            System.out.println("a is greater than or equal to b");
-        }
-
-    }
-
-    public static void forLoop() {
-        int a = 5;
-        for (int i = 0; i < 5; i++) {
-            System.out.println("Value of i: " + i);
-        }
-    }
-
 }
